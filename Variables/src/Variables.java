ec73f9a42d887e98fce7a02ef84e66c24c60933b
Enrique Zepeda <enriquezepeda60z@gmail.com>
2025-01-24 12:18:28 -0600

    Variables

---
+++ Variables/src/Variables.java
@@ -0,0 +1,21 @@
+public class Variables {
+    public static void main(String[] args) {
+       int edad = 31;
+       double precio = 300.40;
+       boolean disponible = true;
+       char genero;
+       genero = 'M';
+
+       //Ver variables
+        System.out.println(edad);
+        System.out.println(precio);
+        System.out.println(disponible);
+        System.out.println(genero);
+
+        //Modificar variables
+        edad = 66;
+        System.out.println(edad);
+
+
+    }
+}

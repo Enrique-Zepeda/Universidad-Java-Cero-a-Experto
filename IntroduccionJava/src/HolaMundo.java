5fb1f13afbfef9b7fe6f167ae4ff15ab3e1a9c74
Enrique Zepeda <enriquezepeda60z@gmail.com>
2025-01-24 11:34:51 -0600

    first commit

---
+++ IntroduccionJava/src/HolaMundo.java
@@ -0,0 +1,7 @@
+public class HolaMundo {
+    // Agregar metodo principal(main)
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo desde Java");
+        System.out.println("Saludos");
+    }
+}
---
+++ IntroduccionJava/src/Presentate.java
@@ -0,0 +1,7 @@
+public class Presentate {
+    public static void main(String[] args) {
+        System.out.println("Nombre: Enrique Adair Zepeda Zambrano");
+        System.out.println("Edad: 22");
+        System.out.println("Pais: Mexico");
+    }
+}

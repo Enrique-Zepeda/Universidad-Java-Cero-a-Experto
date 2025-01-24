aa463e11b570b8b1cb4751e327d5c66f70e234c8
Enrique Zepeda <enriquezepeda60z@gmail.com>
2025-01-24 13:10:39 -0600

    Reto-DetallePersona

---
+++ Variables/src/DetallePersona.java
@@ -0,0 +1,17 @@
+public class DetallePersona {
+    public static void main(String[] args) {
+        //Declaracion de variables
+        String nombre = "Enrique Adair Zepeda Zambrano";
+        int edad = 22;
+        float altura = 1.73F;
+        String pais = "Mexico";
+        char estadoCivil ='C'; //C-casado S-soltero
+
+        //impresion de variables
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.println(altura);
+        System.out.println(pais);
+        System.out.println(estadoCivil);
+    }
+}
